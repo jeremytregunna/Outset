a7c6e19567d99e42c5a8c39674d41c6962b8f75d
Jeremy Tregunna <jeremy@tregunna.ca>
2013-10-30 18:14:56 -0600

    Initial Commit

---
+++ Outset/Outset.h
@@ -0,0 +1,13 @@
+//
+//  Outset.h
+//  Outset
+//
+//  Created by Jeremy Tregunna on 10/30/2013.
+//  Copyright (c) 2013 Jeremy Tregunna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Outset : NSObject
+
+@end
